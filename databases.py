f178b9239ae31e8256133b45ac74517178dd6204
Maria <mariarosekesa@gmail.com>
2023-03-11 00:58:20 +0200

    test

--- databases.py
+++ databases.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import pandas as pd
+
 
 engine = create_engine("sqlite:///test.db",echo=True)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
-#Base.metadata.create_all(engine)
